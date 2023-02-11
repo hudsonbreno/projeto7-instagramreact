a587abfbde3ff7e8fd2292fb3acbcf988b9bf1b1
Breno Hudson <brennoohudson@gmail.com>
2023-02-11 09:55:03 -0300

    componentização

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./NavBar";
+import Corpo from "./Corpo"
+
+
+export default function App(){
+    return(
+      <div>
+          <Navbar/>
+          <Corpo/>
+      </div>
+    );
+  }
+  
